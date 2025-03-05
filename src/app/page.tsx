82ba989173459bb9928d2f43048308b805f7dc83
shunakiya <shunakiya@gmail.com>
2025-03-05 15:18:20 -0800

    still in the middle of designing, need to fix image quality and also figure out what font to use for the left side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,38 @@
 import Image from "next/image";
+import { IoOpenOutline } from "react-icons/io5";
 
 export default function Login() {
   return (
     <div className="relative z-10 flex items-center justify-center min-h-screen">
-      <div className="flex py-3.5 px-3.5 shadow-2xl bg-blue-950 rounded-xl text-left gap-6">
-        <div>
+      <div className="flex py-3.5 px-3.5 shadow-2xl bg-blue-950 rounded-xl text-left gap-6 items-center">
+        <div className="relative h-[550px] w-[450px]">
           <Image
-            src="/assets/images/bg-3.jpg"
-            alt=""
-            className="rounded-lg h-full w-full"
-            width="380"
-            height="0"
+            src="/assets/images/bg-1.jpg"
+            alt="Background 1"
+            className="rounded-lg object-cover object-center"
+            fill
+            sizes="380px"
+            quality={100}
+            priority
           />
+
+          <div className="absolute top-4 right-4 z-10">
+            <a
+              href="https://github.com/shunakiya/SALSNB"
+              target="_blank"
+              className="flex gap-1 rounded-xl bg-white/20 px-3 py-1 text-xs font-light w-fit items-center"
+            >
+              <p className="font-light mt-0.5">GitHub Page</p>
+              <IoOpenOutline size={15} />
+            </a>
+          </div>
+
+          {/* <div className="absolute bottom-2 left-4 right-4">
+            <div className="text-center font-light text-2xl">
+              <p>One Touch,</p>
+              <p>World Unlocked</p>
+            </div>
+          </div> */}
         </div>
 
         <div className="flex flex-col gap-9 w-[400px] px-5 py-10">
@@ -29,12 +50,14 @@ export default function Login() {
             </div>
           </div>
 
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4 text-gray-50/60 ">
             <div className="flex justify-between">
               <input
                 type="text"
                 placeholder="First Name"
-                className="border-none bg-[#353C5A] py-3 px-4 rounded-lg w-[173px] font-light text-sm text-gray-50/60 transition-all ease-in-out duration-200 focus:ring-1 focus:ring-[#708ae6] focus:outline-none"
+                required
+                autoComplete="true"
+                className="border-none bg-[#353C5A] py-2.5 px-4 rounded-lg w-[173px] font-light text-sm transition-all ease-in-out duration-200 focus:ring-1 focus:ring-[#708ae6] focus:outline-none"
               />
               <input
                 type="text"
@@ -56,7 +79,9 @@ export default function Login() {
             />
 
             <div className="w-full items-center bg-[#5D7BE8] text-center py-3 rounded-lg mt-4 transition-all duration-200 ease-in-out hover:bg-[#7591FF] ">
-              <button className="text-sm">Create account</button>
+              <button type="submit" className="text-sm">
+                Create account
+              </button>
             </div>
           </form>
         </div>
--- src/fonts.js
+++ src/fonts.js
@@ -3,22 +3,22 @@ import localFont from "next/font/local";
 export const helvetica = localFont({
   src: [
     {
-      path: "../public/assets/fonts/Helvetica-Light.woff2",
+      path: "../public/assets/fonts/Helvetica/Helvetica-Light.woff2",
       weight: "300",
       style: "normal",
     },
     {
-      path: "../public/assets/fonts/Helvetica-Regular.woff2",
+      path: "../public/assets/fonts/Helvetica/Helvetica-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../public/assets/fonts/Helvetica-RegularItalic.woff2",
+      path: "../public/assets/fonts/Helvetica/Helvetica-RegularItalic.woff2",
       weight: "400",
       style: "italic",
     },
     {
-      path: "../public/assets/fonts/Helvetica-Bold.woff2",
+      path: "../public/assets/fonts/Helvetica/Helvetica-Bold.woff2",
       weight: "700",
       style: "normal",
     },
