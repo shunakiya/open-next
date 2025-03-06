220d0f32e23f2072876abf02b8f2653e3b83847d
shunakiya <shunakiya@gmail.com>
2025-03-06 10:26:07 -0800

    added form validation and light auth

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,20 +1,58 @@
 "use server";
 
-import { RegisterFormSchema } from "@/utils/rules";
+import bcrypt from "bcrypt";
+import { getCollection } from "../utils/db";
+import { RegisterFormSchema } from "../utils/rules";
+import { redirect } from "next/navigation";
 
 export async function register(state: any, formData: any) {
-  const validatredFields = RegisterFormSchema.safeParse({
+  // validate form fields
+  const validatedFields = RegisterFormSchema.safeParse({
     username: formData.get("username"),
     email: formData.get("email"),
     password: formData.get("password"),
   });
 
-  if (!validatredFields.success) {
-    const errorsObject = validatredFields.error.flatten().fieldErrors;
+  // if any form fields are invalid
+  if (!validatedFields.success) {
+    const errorsObject = validatedFields.error.flatten().fieldErrors;
 
     return {
       error: errorsObject,
       email: formData.get("email"),
     };
   }
+
+  // extract form fields
+  const { username, email, password } = validatedFields.data;
+
+  // check if username exists already
+  const userCollection = await getCollection("users");
+  if (!userCollection) {
+    return {
+      error: { username: "Server error." },
+    };
+  }
+
+  const existingUser = await userCollection.findOne({ username });
+  if (existingUser) {
+    return {
+      error: { username: "Username already exists." },
+    };
+  }
+
+  //hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // save in db
+  const results = await userCollection?.insertOne({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  // create a session
+
+  // redirect to login page
+  redirect("/home");
 }
--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,33 +1,33 @@
-// import { MongoClient, ServerApiVersion } from "mongodb";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
-// if (!process.env.DB_URI) {
-//   throw new Error("Mongo URI not found.");
-// }
+if (!process.env.DB_URI) {
+  throw new Error("Mongo URI not found.");
+}
 
-// const client = new MongoClient(process.env.DB_URI, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
+const client = new MongoClient(process.env.DB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-// async function getDB(dbName: string) {
-//   try {
-//     await client.connect();
-//     console.log("db connected.");
-//     return client.db(dbName);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
+async function getDB(dbName: string) {
+  try {
+    await client.connect();
+    console.log("CONNECTED TO DB");
+    return client.db(dbName);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-// export async function getCollection(collectionName: string) {
-//   const db = await getDB("open-next-db");
+export async function getCollection(collectionName: string) {
+  const db = await getDB("open-app");
 
-//   if (db) {
-//     return db.collection(collectionName);
-//   }
+  if (db) {
+    return db.collection(collectionName);
+  }
 
-//   return null;
-// }
+  return null;
+}
