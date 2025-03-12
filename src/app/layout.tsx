9455ea4bdcbaed33fd46b3b7d3573bc4aceffc8d
shunakiya <shunakiya@gmail.com>
2025-03-12 13:51:43 -0700

    added more metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,44 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import { satoshi } from "../fonts";
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
 
 export const metadata: Metadata = {
   title: "open",
   description: "Effortless access, and unlock your world with a tap or touch.",
+  keywords: "lock, smart lock, fingerprint, nfc, cse5408, cse5208, salsnb",
+  authors: [{ name: "Shun Akiya, Christopher Cao, Rafael Ceja, Matthew Uy" }],
+  creator: "Shun Akiya",
+  publisher: "Shun Akiya",
+  openGraph: {
+    title: "open",
+    description:
+      "Effortless access, and unlock your world with a tap or touch.",
+    url: "https://un-lock.vercel.app",
+    siteName: "open",
+    images: [
+      {
+        url: "https://un-lock.vercel.app/images/preview.png",
+        width: 1200,
+        height: 600,
+        alt: "open App Preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "open",
+    description:
+      "Effortless access, and unlock your world with a tap or touch.",
+    images: ["https://ponder-it.vercel.app/images/preview.png"],
+  },
+  robots: "index, follow",
 };
 
 export default function RootLayout({
